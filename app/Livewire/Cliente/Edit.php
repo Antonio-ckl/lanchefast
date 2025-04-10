3fd04a6bf5c828fa15a4c17aeaf28e236b974721
Antonio-ckl <antonio.cklebis@gmail.com>
2025-04-10 15:17:48 -0300

    Editar cliente

--- app/Livewire/Cliente/Edit.php
+++ app/Livewire/Cliente/Edit.php
@@ -2,10 +2,42 @@
 
 namespace App\Livewire\Cliente;
 
+use App\Models\Cliente;
 use Livewire\Component;
 
 class Edit extends Component
 {
+
+    public $nome;
+    public $endereco;
+    public $telefone;
+    public $email;
+    public $cpf;
+    public $senha;
+    
+    public function mount($id){
+        $clientes=Cliente::find($id);
+
+        $this->nome->$clientes->nome;
+        $this->endereco->$clientes->endereco;
+        $this->telefone->$clientes->telefone;
+        $this->email->$clientes->email;
+        $this->cpf->$clientes->cpf;
+        $this->senha->$clientes->senha;
+    }
+
+    public function save(){
+        $clientes= Cliente::find($this->id);
+        $this->nome->$clientes->nome;
+        $this->endereco->$clientes->endereco;
+        $this->telefone->$clientes->telefone;
+        $this->email->$clientes->email;
+        $this->cpf->$clientes->cpf;
+        $this->senha->$clientes->senha;
+
+        $clientes->save();
+        session()->flash('success', 'Cliente Atualizado');
+    }
     public function render()
     {
         return view('livewire.cliente.edit');
