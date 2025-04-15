f4066b7b9b4266c40c1ab704e1f520f9a6d5f694
Antonio-ckl <antonio.cklebis@gmail.com>
2025-04-15 10:51:09 -0300

    create blade

--- resources/views/livewire/cliente/create.blade.php
+++ resources/views/livewire/cliente/create.blade.php
@@ -1,11 +1,11 @@
-<div class="container mt-4">
+<div class="container mt-5 ">
+    <div class="bg-transparent border-0 d-flex justify-content-between align-items-center mb-4 pt-3">
+        <h1 class="h3 text-primary"><i class="bi bi-person-circle"></i> Cadastrar Cliente</h1>
+        <a href="{{ route('cliente.index') }}" class="btn  btn-outline-secondary">
+            <i class="bi bi-arrow-left"></i> Voltar
+        </a>
+    </div>
     <div class="card">
-        <div class="card-header d-flex justify-content-between align-items-center">
-            <h2>Cadastrar Novo Cliente</h2>
-            <a href="{{ route('cliente.index') }}" class="btn btn-secondary">
-                <i class="bi bi-arrow-left"></i> Voltar
-            </a>
-        </div>
         
         <div class="card-body">
             @if(session('message'))
