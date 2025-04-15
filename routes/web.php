570bd5fdef635776f914839aa3f69b3585aacbac
Antonio-ckl <antonio.cklebis@gmail.com>
2025-04-15 10:51:24 -0300

    web php

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('clientes')->group(function(){
     Route::get('/', App\Livewire\Cliente\Index::class)->name('cliente.index');
     Route::get('/create', App\Livewire\Cliente\Create::class)->name('cliente.create');
-    Route::get('/{cliente}', App\Livewire\cliente\show::class)->name('cliente.show');
+    Route::get('/{id}', App\Livewire\cliente\show::class)->name('cliente.show');
     Route::get('/{id}/edit', App\Livewire\Cliente\Edit::class)->name('cliente.edit');
 });
 
