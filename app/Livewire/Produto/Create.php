ff5cedbef15d1a0dffeb5bd1b8a3b137b69c5b44
Antonio-ckl <antonio.cklebis@gmail.com>
2025-04-15 12:27:31 -0300

    Produto Create

--- app/Livewire/Produto/Create.php
+++ app/Livewire/Produto/Create.php
@@ -2,10 +2,34 @@
 
 namespace App\Livewire\Produto;
 
+use App\Models\Produto;
 use Livewire\Component;
 
 class Create extends Component
 {
+    public $nome;
+    public $ingredientes;
+    public $valor;
+    public $imagem;
+
+    protected $rules =[
+        'nome'=> 'required',
+        'ingredientes' => 'required',
+        'valor' => 'required',
+    ];
+    public function store(){
+        $this->validate();
+
+        Produto::create([
+            'nome'=>$this->nome,
+            'ingredientes'=>$this->ingredientes,
+            'valor'=>$this->valor,
+            'imagem'=>$this->imagem
+        ]);
+
+        session()->flash('message', 'Produto cadastrado com sucesso!');
+        return redirect()->to('/produtos');
+    }
     public function render()
     {
         return view('livewire.produto.create');
