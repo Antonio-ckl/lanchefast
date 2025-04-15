4e743f4219123dcde8eb7ea4f7b2915257f6fc09
Antonio-ckl <antonio.cklebis@gmail.com>
2025-04-15 10:48:06 -0300

    Inserção do Show

--- app/Livewire/Cliente/Show.php
+++ app/Livewire/Cliente/Show.php
@@ -2,10 +2,31 @@
 
 namespace App\Livewire\Cliente;
 
+use App\Models\Cliente;
 use Livewire\Component;
 
 class Show extends Component
 {
+    public $clienteId;
+    public $nome;
+    public $endereco;
+    public $telefone;
+    public $email;
+    public $cpf;
+    public $senha;
+    
+    public function mount($id)
+    {
+        $cliente = Cliente::findOrFail($id);
+        $this->clienteId = $cliente->id;
+        $this->nome = $cliente->nome;
+        $this->endereco = $cliente->endereco;
+        $this->telefone = $cliente->telefone;
+        $this->email = $cliente->email;
+        $this->cpf = $cliente->cpf;
+        $this->senha = $cliente->senha;
+    }
+
     public function render()
     {
         return view('livewire.cliente.show');
